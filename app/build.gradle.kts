a749ef52156dadaaec635872e6233ae2a5104d98
Theo-17 <mateo.espinoza@epn.edu.ec>
2025-07-17 08:46:24 -0500

    Integración de Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+    //id("com.android.application")
+    //id("com.google.gms.google-services")
 }
 
 android {
@@ -42,7 +45,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    //implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform(libs.firebase.bom))
+    // Declare the dependencies for the desired Firebase products, without specifying versions
+    // For example, declare the dependencies for Firebase Authentication and Cloud Firestore
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore.ktx)
+
 }
--- app/src/main/java/com/edison/jimenez/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/edison/jimenez/cazarpatos/LoginActivity.kt
@@ -3,17 +3,23 @@ package com.edison.jimenez.cazarpatos
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     lateinit var editTextEmail: EditText
-    lateinit var editTextPassword:EditText
+    lateinit var editTextPassword: EditText
     lateinit var buttonLogin: Button
-    lateinit var buttonNewUser:Button
+    lateinit var buttonNewUser: Button
     lateinit var mediaPlayer: MediaPlayer
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,21 +39,27 @@ class LoginActivity : AppCompatActivity() {
             val email = editTextEmail.text.toString()
             val clave = editTextPassword.text.toString()
             //Validaciones de datos requeridos y formatos
-            if(!validateRequiredData())
+            if (!validateRequiredData())
                 return@setOnClickListener
             //Si pasa validación de datos requeridos, ir a pantalla principal
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra(EXTRA_LOGIN, email)
-            startActivity(intent)
-            finish()
+            //val intent = Intent(this, MainActivity::class.java)
+            //intent.putExtra(EXTRA_LOGIN, email)
+            //startActivity(intent)
+            //finish()
+            AutenticarUsuario(email, clave)
         }
-        buttonNewUser.setOnClickListener{
+        buttonNewUser.setOnClickListener {
 
         }
-        mediaPlayer=MediaPlayer.create(this, R.raw.title_screen)
+        mediaPlayer = MediaPlayer.create(this, R.raw.title_screen)
         mediaPlayer.start()
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
     }
-    private fun validateRequiredData():Boolean{
+
+    private fun validateRequiredData(): Boolean {
         val email = editTextEmail.text.toString()
         val password = editTextPassword.text.toString()
         if (email.isEmpty()) {
@@ -67,8 +79,30 @@ class LoginActivity : AppCompatActivity() {
         }
         return true
     }
+
     override fun onDestroy() {
         mediaPlayer.release()
         super.onDestroy()
+    }
+
+    fun AutenticarUsuario(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(EXTRA_LOGIN, "signInWithEmail:success")
+                    //Si pasa validación de datos requeridos, ir a pantalla principal
+                    val intencion = Intent(this, MainActivity::class.java)
+                    intencion.putExtra(EXTRA_LOGIN, auth.currentUser!!.email)
+                    startActivity(intencion)
+                    //finish()
+                } else {
+                    Log.w(EXTRA_LOGIN, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, task.exception!!.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
     }
 }
